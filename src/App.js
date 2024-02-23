031e99cb885a6a0ee89c7c7968a679d3e3a67e72
vijaylaxmi hanamshetty <vijaylaxmihanamshetty38@.in>
2024-02-23 06:41:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+import Typography from '@mui/material/Typography'
 function App() {
+  const [datas, setDatas] = useState([]);
+  
+  useEffect(() => {
+    const apiUrl = "https://jsonplaceholder.typicode.com/posts/1/comments";
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setDatas(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  
+         
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1  style={{textAlign:'center'}}>User Data</h1>
+      
+       
+      {datas.map(data => (
+        <Card sx={{ minWidth: 275 }} style={{margin:'20px'}}>
+        <CardContent key={data.id}>
+        <Typography gutterBottom variant="h4" component="div">
+        {data.name}
+        </Typography>
+        <Typography variant="h4" component="h2">
+  {data.email}
+</Typography>
+        <Typography variant="body2" color="text.secondary">
+         {data.body}
+        </Typography>
+        </CardContent>
+        </Card>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default App
+          
+        
+        
+        
+
+  
+
+        
